daedc16e815b5eee4edc89354f3efa32b5f2469d
Roger <roger.freitas@supera.com.br>
2023-03-31 12:49:20 -0300

    Upload da estrutura inicial do core do projeto

---
+++ src/test/java/com/expensecontrol/ExpansecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpansecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
